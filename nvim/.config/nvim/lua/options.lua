6b59a9658fd33f409cb7832f3d1958135b787fb5
dyrattanak <rattanak@e.dynamo-tech.com>
2024-06-26 17:11:08 +0700

    Set tabwidth

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -7,4 +7,10 @@ vim.opt.breakindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.scrolloff = 10
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- vim: ts=2 sts=2 sw=2 et
